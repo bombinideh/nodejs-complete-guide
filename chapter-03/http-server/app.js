a1afd66a08f9bce6bd38099b155584d605a94d60
Deborah Bombini <dehhbombini@gmail.com>
2023-02-20 14:10:50 -0300

    adicionado routes

--- chapter-03/http-server/app.js
+++ chapter-03/http-server/app.js
@@ -1,41 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const { method, url } = req;
+const routes = require("./routes");
 
-  if (url === "/" && method === "GET") {
-    res.write("<html>");
-    res.write("<body>");
-    res.write('<form method="post">');
-    res.write('<input type="text" name="message" />');
-    res.write('<button type="submit">Enviar</button>');
-    res.write("</form>");
-    res.write("</body>");
-    res.write("</html>");
-    res.end();
-  } else if (url === "/" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        if (err) {
-          console.error(err);
-          res.statusCode = 500;
-          res.end();
-          return;
-        }
-
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
